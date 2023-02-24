5440e0bc0273f1c4367ad2437f712b7344c9c5d2
hyoudouzixi <wang.13099@buckeyemail.osu.edu>
2023-02-24 09:52:39 -0500

    update on project1

--- Project1/script.js
+++ Project1/script.js
@@ -67,3 +67,27 @@ function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
     
 }
+
+
+
+document.addEventListener("mousedown", rotateCube);
+
+function rotateCube(e) {
+  
+  var cube = document.getElementsByClassName("cube");
+  var x = e.clientX - window.innerWidth / 2;
+  var y = e.clientY - window.innerHeight / 2;
+  var q = 0.15;
+  var i;
+
+  x = x * q * 1.25;
+  y = -y * q * 1.25;
+
+  for(i = 0; i < cube.length; i++) {
+    cube[i].style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)";
+  }
+
+}
+
+
+
